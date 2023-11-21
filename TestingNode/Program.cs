7bd63d72ff9599a16d26ede3ed0332321b6eb2ed
Michael <avilinmc@gmail.com>
2023-11-21 12:59:54 -0600

    new changes

---
+++ TestingNode/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Cringe ahhh template");
+Console.ReadLine();
+
+public class doMath
+{
+    int num1 = 0;
+    int num2 = 0;
+}
