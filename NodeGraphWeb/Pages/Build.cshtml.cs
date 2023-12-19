6d434bcfeee113eb521b0a061c8379bc38b5fbd9
Michael <avilinmc@gmail.com>
2023-12-19 11:18:15 -0600

    WP-72 Implementations.

--- NodeGraphWeb/Pages/Build.cshtml.cs
+++ NodeGraphWeb/Pages/Build.cshtml.cs
@@ -1,12 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Msagl.Drawing;
+using NodeGraphWeb.Services;
 
 namespace NodeGraphWeb.Pages
 {
-    public class Index1Model : PageModel
+    public class GraphModel2 : PageModel
     {
+        private readonly GraphService _graphService;
+
+        public string? GraphJsonData { get; private set; }
+
+        public GraphModel2(GraphService graphService2)
+        {
+            _graphService2 = graphService2;
+        }
+
         public void OnGet()
         {
+            //Fetch the graph data in JSON format from the database
+            GraphJsonData = _graphService.GetGraphDataFromDatabase();
+
+            /* // previous code for fetching directly from backend
+            var graph = _graphService.BuildGraph("https://github.com");
+            GraphJsonData = _graphService.ConvertGraphToJson(graph);
+            */
         }
     }
 }
---
+++ NodeGraphWeb/Services/Index1Model.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.Json; // For JSON serialization
+using Microsoft.Msagl.Drawing;
+using NodeGraphWeb.Services;
+
+namespace NodeGraphWeb.Services
+{
+    // uncomment this code if you want links to be fetched directly from backend calculations
+    
+    public class GraphServiceBuild
+    {
+        public GraphServiceBuild()
+        {
+            // Initialization, if necessary
+        }
+
+        public Graph BuildGraph(string url)
+        {
+            WebGraph webGraph = new WebGraph();
+            // Use methods from your NodeGraph project
+            webGraph.BuildGraph(url);
+            return webGraph.Visualize(); // Assuming this is how you build and visualize the graph
+        }
+
+        public string ConvertGraphToJson(Graph graph)
+        {
+            var nodes = graph.Nodes.Select(n => new {
+                Id = n.Id, // this is the node id 
+                Label = n.LabelText, // this is the text
+                Color = $"#{n.Attr.FillColor.R:X2}{n.Attr.FillColor.G:X2}{n.Attr.FillColor.B:X2}", // this is the color 
+                Size = n.Label.FontSize // this is the size 
+            });
+            var edges = graph.Edges.Select(e => new {
+                source = e.SourceNode.Id,
+                target = e.TargetNode.Id
+            });
+
+
+            var graphData = new { Nodes = nodes, Edges = edges };
+            return System.Text.Json.JsonSerializer.Serialize(graphData);
+        }
+    }
+}
